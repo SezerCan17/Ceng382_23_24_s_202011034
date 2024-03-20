67a46041adceb4706b56602e1c400bc328891611
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2024-03-20 12:14:01 +0300

    Lab5 commit

---
+++ lab4/Pages/nameOfThePage.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class nameOfThePageModel : PageModel
+    {
+        private readonly ILogger<nameOfThePageModel> _logger;
+
+    public nameOfThePageModel(ILogger<nameOfThePageModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    }
+}
