bda330309c633d191bb599d5fd6fa6e3b53f2a29
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2024-03-27 11:42:32 +0300

    lab

--- lab4/obj/Debug/net8.0/lab4.AssemblyInfo.cs
+++ lab4/obj/Debug/net8.0/lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6d5bc5c11cdc17b0c4e4f73d6b11770e1a15ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1697041bcf3e8347fc6c181d97e55e13d53746")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
