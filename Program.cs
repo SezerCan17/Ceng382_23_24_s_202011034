56e692d21c9b9accf328b92156c2c5e75b7ca148
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2024-04-02 21:56:37 +0300

    Lab6

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class RoomData
+{
+
+    [JsonPropertyName("Room")]
+    public Room[]? Rooms { get; set; }
+}
+
+public class Room
+{
+
+
+    [JsonPropertyName("roomId")]
+    public string roomId { get; set; }
+
+    [JsonPropertyName("roomName")]
+    public string roomName { get; set; }
+
+
+
+    [JsonPropertyName("capacity")]
+    public int capacity { get; set; }
+
+}
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        
+
+        
+
+        //path to json
+        string jsonFilePath = "Data.json";
+        string jsonString = File.ReadAllText(jsonFilePath);
+
+        //options to read
+        var options = new JsonSerializerOptions()
+        {
+            NumberHandling = JsonNumberHandling.AllowReadingFromString |
+            JsonNumberHandling.WriteAsString
+        };
+
+
+        var roomData = JsonSerializer.Deserialize<RoomData>(jsonString, options);
+
+        if (roomData?.Rooms != null)
+        {
+            foreach (var room in roomData.Rooms)
+            {
+                Console.WriteLine($"Room ID : {room.roomId}, Name:{room.roomName},Room ID : {room.roomId}");
+            }
+        }
+
+        ReservationHandler reservationHandler;
+        
+        reservationHandler.addReservation(n);
+
+    }
+   
+}
+
+
+class ReservationHandler
+{
+    private Reservation[,] reservation = new Reservation[10,10];
+    
+
+    
+    public void addReservation(Reservation reservation)
+    {
+        Console.WriteLine("SA");
+
+
+
+    }
+
+    public void deleteReservation(Reservation[,] reservation)
+    {
+
+    }
+
+    public void displayWeeklySchedule()
+    {
+
+    }
+}
+
+class Reservation
+{
+    
+   public DateTime time;
+public    DateTime date;
+   public string reserverName;
+
+    Room room;
+        
+}
+
+
